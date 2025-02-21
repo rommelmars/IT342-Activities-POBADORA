82b75ee1910fa5d9ef53a457a57899c2c36c9f19
LAB14_USER ON WS21 <L14X20W21@citulab.net>
2025-02-21 18:42:02 +0800

    Initial commit G7

---
+++ g7-pobadora-rommel/oauth2login/src/test/java/com/pobadora/oauth2login/OAuth2loginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pobadora.oauth2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2loginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
